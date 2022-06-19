1f6dde7b77784397bd61f2db7334ddb3baed34f1
FoRPoP <bapop413@gmail.com>
2022-06-19 14:57:47 -0700

    Fixed bug where it was possible to schedule meeting on a blank day. Removed text highlighting when double clicking to schedule a meeting

--- frontend/src/components/Day/index.js
+++ frontend/src/components/Day/index.js
@@ -2,17 +2,29 @@ import styles from './day.module.css'
 
 const Day = ({date, modal, meetings}) => 
 {
-    return(
-        <div className = {styles.day} onDoubleClick = {modal}>
-            <h1>{date}</h1>
-            {
-                meetings.map((meeting) =>
+    if (date === ' ')
+    {
+        return(
+            <div className = {styles.day}>
+                <h1>{date}</h1>
+            </div>
+        )
+    }
+    else
+    {
+        return(
+            <div className = {styles.day} onDoubleClick = {modal}>
+                <h1>{date}</h1>
                 {
-                    return <p>{meeting.title} : {meeting.time}</p>
-                })
-            }
-        </div>
-    )
+                    meetings.map((meeting) =>
+                    {
+                        return <p>{meeting.title} : {meeting.time}</p>
+                    })
+                }
+            </div>
+        )
+    }
+    
 }
 
 export default Day;
